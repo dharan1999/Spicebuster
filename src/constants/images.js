23b470867a902ab52c825b89248e22900b5a1195
dharan1999 <dharanthakar96@gmail.com>
2023-07-07 17:00:02 -0400

    Changed Background and added links

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,7 @@
 import bg from '../assets/bg.png';
+import bg1 from '../assets/bg1.jpg';
 import chef from '../assets/chef.png';
+import pin from '../assets/pin.png';
 import G from '../assets/G.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
@@ -20,6 +22,11 @@ import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
+import paneer from '../assets/paneer.jpg';
+import eggbiryani from '../assets/Egg-Biryani.jpg';
+import puff from '../assets/puff.jpg';
+import sevpuri from '../assets/puri.jpg';
+import samosa from '../assets/samosa.jpg';
 
 export default {
   bg,
@@ -44,4 +51,10 @@ export default {
   sign,
   quote,
   gericht,
+  paneer,
+  sevpuri,
+  puff,
+  samosa,
+  eggbiryani,
+  pin,
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -17,13 +17,30 @@ const Header = () => {
   }, []);
 
   const imageList = [
-    images.welcome,
-    images.gallery01,
-    images.gallery02,
-    images.gallery03,
-    images.gallery04,
+    images.paneer,
+    images.eggbiryani,
+    images.puff,
+    images.sevpuri,
+    images.samosa,
   ];
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const targetLocation = '#menu';
+  
+    if (window.location.hash === targetLocation) {
+      // Target location is already in the URL, scroll to it
+      const targetElement = document.querySelector(targetLocation);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      // Target location is not in the URL, update the hash and prevent default
+      window.location.hash = targetLocation;
+    }
+  };
   return (
+
     <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
         <SubHeading title="Chase the new flavour" />
@@ -31,18 +48,16 @@ const Header = () => {
         <p className="p__opensans" style={{ margin: '2rem 0' }}>
           Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
         </p>
-        <button type="button" className="custom__button">
+        <button type="button" className="custom__button" onClick={handleClick}>
           Explore Menu
         </button>
       </div>
 
       <div className="app__wrapper_img">
-        <img src={imageList[imageList.length == 5 ? currentImageIndex % 5 : currentImageIndex]} alt="header_img" />
+        <img src={imageList[imageList.length === 5 ? currentImageIndex % 5 : currentImageIndex]} alt="header_img" />
       </div>
     </div>
   );
 };
 
 export default Header;
-
-
